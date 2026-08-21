0d7d8bcc18bb57c2602b9e2d28813c54f1d9e6be
iamJABASTIN <itsmejabastin@gmail.com>
2026-08-21 16:57:30 +0530

    feat: new placeholder pages  were added

---
+++ client/src/app/pages/customizer/customizer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customizer',
+  standalone: true,
+  templateUrl: './customizer.component.html',
+  styleUrl: './customizer.component.css'
+})
+export class CustomizerComponent {}
